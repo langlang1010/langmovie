1b3aecca5e538571f911ca7ad1b38515445a72e4
langlang1010 <3225910491@qq.com>
2019-04-10 15:24:44 +0800

    首次创建后台项目

---
+++ langmovie/src/main/java/cn/langlang/langmovie/LangmovieApplication.java
@@ -0,0 +1,13 @@
+package cn.langlang.langmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangmovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangmovieApplication.class, args);
+    }
+
+}
